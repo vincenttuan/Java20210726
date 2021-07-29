d96a372fceed13de2247ca50f13b4d6ab58e5dfd
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-29 15:19:10 +0800

    java

---
+++ study/src/main/java/com/study/day11/MultiArrayJava8Demo3.java
@@ -0,0 +1,17 @@
+package com.study.day11;
+
+public class MultiArrayJava8Demo3 {
+
+	public static void main(String[] args) {
+		Exam e1 = new Exam("Java", 90);
+		Exam e2 = new Exam("Java", 80);
+		Exam e3 = new Exam("Java", 100);
+		Exam e4 = new Exam("Java", 40);
+		Exam e5 = new Exam("VB", 60);
+		// 請問此Java考生的平均分數 ? 最高最低分各是多少 ?
+		// 用 Java 8
+		Exam[] exams = {e1, e2, e3, e4, e5};
+		
+	}
+
+}
