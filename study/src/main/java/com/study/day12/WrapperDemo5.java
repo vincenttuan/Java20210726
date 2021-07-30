7f04b800645c8eaac2e1cd58eb5da197881735e5
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-30 13:20:26 +0800

    java

---
+++ study/src/main/java/com/study/day12/WrapperDemo5.java
@@ -0,0 +1,18 @@
+package com.study.day12;
+
+public class WrapperDemo5 {
+
+	public static void main(String[] args) {
+		Employee e1 = new Employee("John", 20, 40000);
+		Employee e2 = new Employee("Mary", 30, 50000);
+		Employee e3 = new Employee("Helen", 25, null);
+		Employee e4 = new Employee("Jim", 40, 60000);
+		Employee e5 = new Employee("Jack", 35, 55000);
+		
+		// 試問公司平均年齡 = ?
+		
+		
+		
+	}
+
+}
