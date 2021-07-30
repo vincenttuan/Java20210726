c97491c93e64760d41cdafd073fcec16748623c8
vincenttuan <vincentjava@yahoo.com.tw>
2021-07-30 10:24:46 +0800

    java

---
+++ study/src/main/java/com/study/day11/MultiArrayJava8Demo9.java
@@ -0,0 +1,16 @@
+package com.study.day11;
+
+public class MultiArrayJava8Demo9 {
+	public static void main(String[] args) {
+		Exam e1 = new Exam("Java", 90);
+		Exam e2 = new Exam("Java", 80);
+		Exam e3 = new Exam("Java", 100);
+		Exam e4 = new Exam("Java", 40);
+		Exam e5 = new Exam("VB", 60);
+		
+		Exam[][] exams = { {e1, e2} , {e3, e4}, {e5} };
+		// 不分科目，求總分 = ?
+		
+		
+	}
+}
