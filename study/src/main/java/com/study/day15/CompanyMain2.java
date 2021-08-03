557d0f4b946e1f680f5a98526e7a82ff6865184b
MB-teacher <MB-teacher@MB-207-teacher.lan>
2021-08-03 10:36:49 +0800

    java

--- study/src/main/java/com/study/day15/CompanyMain2.java
+++ study/src/main/java/com/study/day15/CompanyMain2.java
@@ -9,21 +9,24 @@ public class CompanyMain2 {
 		employee.salary = 4_0000;
 		manager.salary = 7_0000;
 		supervisor.salary = 15_0000;
+		manager.budget = 20_0000;
+		supervisor.budget = 100_0000;
 		// 調用(呼叫)列印薪資單方法
 		printSalary(employee);
 		printSalary(manager);
 		printSalary(supervisor);
+		// 查看預算
+		printBudget(manager);
+		printBudget(supervisor);
 	}
 	
 	// 列印薪資單
 	public static void printSalary(Employee employee) {
 		System.out.printf("列印員工薪資單: $%,d\n", employee.salary);
 	}
-	public static void printSalary(Manager manager) {
-		System.out.printf("列印員工薪資單: $%,d\n", manager.salary);
+	// 查看預算
+	public static void printBudget(Manager manager) {
+		System.out.printf("查看預算: $%,d\n", manager.budget);
 	}
-	public static void printSalary(Supervisor supervisor) {
-		System.out.printf("列印員工薪資單: $%,d\n", supervisor.salary);
-	}
-
+	
 }
