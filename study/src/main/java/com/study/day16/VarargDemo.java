c79d4f25316bf716d8ce6f1d59c63db77ca16532
vincent <vincentjava@yahoo.com.tw>
2021-08-04 10:35:47 +0800

    java

--- study/src/main/java/com/study/day16/VarargDemo.java
+++ study/src/main/java/com/study/day16/VarargDemo.java
@@ -3,6 +3,7 @@ package com.study.day16;
 public class VarargDemo {
 	
 	public static int add(int... array) {
+		System.out.println(array + " 長度:" + array.length);
 		int sum = 0;
 		for(int x : array) {
 			sum += x;
@@ -14,9 +15,11 @@ public class VarargDemo {
 		int[] array = {10, 20, 30};
 		System.out.println(add(array));
 		
-		System.out.println(add(10, 20));
+		System.out.println(add(10, 20, 30)); // new int[]{10, 20, 30}
 		
-		System.out.println(add(10, 20, 30, 40));
+		System.out.println(add(10, 20)); // new int[]{10, 20}
+		
+		System.out.println(add(10, 20, 30, 40)); // new int[]{10, 20, 30, 40}
 		
 		System.out.println(add(10));
 		
