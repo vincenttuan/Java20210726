adc10ca655b7e2b47bad14848d8a896551d7b144
vincent <vincentjava@yahoo.com.tw>
2021-08-05 09:32:47 +0800

    java

--- study/src/main/java/com/study/day22/Main.java
+++ study/src/main/java/com/study/day22/Main.java
@@ -1,11 +1,17 @@
 package com.study.day22;
 
 class Outer {
-	
+	int x = 7; // Outer's 物件變數
 	class Inner {
+		int x = 77; // Inner's 物件變數
 		void job() {
+			int x = 777; // job() 方法的區域變數
 			System.out.println("java...");
+			System.out.println("區域變數 x = " + x);
+			System.out.println("Inner's 物件變數 x = " + this.x);
+			System.out.println("Outer's 物件變數 x = " + Outer.this.x);
 		}
+		
 	}
 	
 }
