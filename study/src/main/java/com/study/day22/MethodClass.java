5f2a0e2aad107793d4a068077190f4a0e32aa8c9
vincent <vincentjava@yahoo.com.tw>
2021-08-05 10:01:23 +0800

    java

--- study/src/main/java/com/study/day22/MethodClass.java
+++ study/src/main/java/com/study/day22/MethodClass.java
@@ -3,18 +3,28 @@ package com.study.day22;
 class Job {
 	// 方法
 	void start() {
+		int year = 2021; // 方法區域變數
+		                 // 若此變數要給方法內部類別
+		                 // 則編譯器會在背後加上 final 來修飾
 		// 方法內部類別
 		class Report {
-			
 			void content() {
-				System.out.println("年度報告");
+				System.out.println(year + "年度報告");
 			}
-			
 		}
 		// 建立
 		Report report = new Report();
 		report.content();
 	}
+	
+	void other() {
+		
+		class Report {
+			
+		}
+		
+	}
+	
 }
 
 public class MethodClass {
