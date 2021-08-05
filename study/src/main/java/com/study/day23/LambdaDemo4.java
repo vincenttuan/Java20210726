ed421e082f88770261049573af5193cdf5a724f5
vincent <vincentjava@yahoo.com.tw>
2021-08-05 14:41:28 +0800

    java

--- study/src/main/java/com/study/day23/LambdaDemo4.java
+++ study/src/main/java/com/study/day23/LambdaDemo4.java
@@ -1,12 +1,21 @@
 package com.study.day23;
 
+import java.util.Arrays;
+
 public class LambdaDemo4 {
 
 	public static void main(String[] args) {
 		Object[] products = {"牛奶", 100, "咖啡", 50};
 		// 請問此次消費金額 ?
 		
-
+		int sum = Arrays.stream(products)
+				//.peek(p -> System.out.println(p + ", " + p.getClass().getSimpleName()))
+				.filter(p -> p instanceof Integer) // Stream 
+				//.peek(p -> System.out.println(p + ", " + p.getClass().getSimpleName()))
+				.mapToInt(p -> ((Integer)p).intValue()) // IntStream
+				//.peek(p -> System.out.println(p))
+				.sum();
+		System.out.println(sum);
 	}
 
 }
