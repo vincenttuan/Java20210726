c6eef41679215bcb557c838669d9bd5020bbd338
vincent <vincentjava@yahoo.com.tw>
2021-08-05 11:33:51 +0800

    java

--- study/src/main/java/com/study/day22/LambdaDemo.java
+++ study/src/main/java/com/study/day22/LambdaDemo.java
@@ -11,7 +11,14 @@ public class LambdaDemo {
 			}
 		};
 		System.out.println(add1.calc(10, 20));
-
+		
+		// Lambda
+		Add add2 = (int x, int y) -> x + y;
+		Add add3 = (x, y) -> x + y;
+		
+		System.out.println(add2.calc(10, 20));
+		System.out.println(add3.calc(10, 20));
+		
 	}
 
 }
