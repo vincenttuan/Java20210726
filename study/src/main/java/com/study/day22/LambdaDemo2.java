5b3bfa30bf36f50d3b73ed548e468f9d3a5deabf
vincent <vincentjava@yahoo.com.tw>
2021-08-05 11:39:03 +0800

    java

---
+++ study/src/main/java/com/study/day22/LambdaDemo2.java
@@ -0,0 +1,13 @@
+package com.study.day22;
+
+public class LambdaDemo2 {
+
+	public static void main(String[] args) {
+		// score : 75
+		// score : 45
+		// 直接用 lambda 語法求解 ?
+		
+
+	}
+
+}
---
+++ study/src/main/java/com/study/day22/Score.java
@@ -0,0 +1,7 @@
+package com.study.day22;
+
+// 成績
+public interface Score {
+	// 判斷成績是否及格 ?
+	boolean isPassOrFail(int score);
+}
