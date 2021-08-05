4c6e50ccc0938ad92b0967bb0774ab8d1208267b
vincent <vincentjava@yahoo.com.tw>
2021-08-05 10:36:24 +0800

    java

--- study/src/main/java/com/study/day22/CircleAreaMain.java
+++ study/src/main/java/com/study/day22/CircleAreaMain.java
@@ -4,7 +4,18 @@ public class CircleAreaMain {
 
 	public static void main(String[] args) {
 		// 若半徑 = 10, 求面積 = ?
+		CircleArea circleArea = new CircleArea() {
+
+			@Override
+			public double calc(double r) {
+				//double result = r * r * 3.14;
+				double result = Math.pow(r, 2) * Math.PI;
+				return result;
+			}
+			
+		};
 		
+		System.out.println(circleArea.calc(10));
 
 	}
 
