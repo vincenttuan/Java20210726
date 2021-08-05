564c02a3f1ab724c788df7f8e8ffa82b3e5a85ff
vincent <vincentjava@yahoo.com.tw>
2021-08-05 10:28:18 +0800

    java

---
+++ study/src/main/java/com/study/day22/CircleArea.java
@@ -0,0 +1,7 @@
+package com.study.day22;
+
+// 圓面積
+public interface CircleArea {
+	// 計算 (半徑 = r)
+	double calc(double r);
+}
---
+++ study/src/main/java/com/study/day22/CircleAreaMain.java
@@ -0,0 +1,10 @@
+package com.study.day22;
+
+public class CircleAreaMain {
+
+	public static void main(String[] args) {
+		// 若半徑 = 10, 求面積 = ?
+
+	}
+
+}
