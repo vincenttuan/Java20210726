d8f22ad8af82a74073a364d68720f6fb60b4e2fd
vincent <vincentjava@yahoo.com.tw>
2021-08-06 13:53:26 +0800

    java

---
+++ study/src/main/java/com/study/day25/QueueDemo.java
@@ -0,0 +1,24 @@
+package com.study.day25;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+	public static void main(String[] args) {
+		// 先進先出
+		Queue<String> queue = new LinkedList<>();
+		queue.offer("紅茶");
+		queue.offer("奶茶");
+		queue.offer("咖啡");
+		System.out.println(queue);
+		// 取出
+		while(!queue.isEmpty()) {
+			// 喝飲料
+			String name = queue.poll();
+			// 剩餘飲料
+			System.out.printf("喝: %s 剩下: %s\n", name, queue);
+		}
+		
+		System.out.printf("最後: %s\n", queue);
+	}
+}
