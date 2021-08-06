a097cd17e4a340541ca0450864c21fd46d46abc0
vincent <vincentjava@yahoo.com.tw>
2021-08-06 10:23:50 +0800

    java

---
+++ study/src/main/java/com/study/day24/EqualsBookDemo.java
@@ -0,0 +1,24 @@
+package com.study.day24;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class EqualsBookDemo {
+
+	public static void main(String[] args) {
+		Book b1 = new Book("Java", "Vincent", 200);
+		Book b2 = new Book("Python", "Anita", 250);
+		Book b3 = new Book("Java", "Vincent", 200);
+		Book b4 = new Book("C#", "Helen", 150);
+		
+		Set<Book> books = new LinkedHashSet<>();
+		books.add(b1);
+		books.add(b2);
+		books.add(b3);
+		books.add(b4);
+		System.out.println(books);
+		System.out.println(books.size());
+
+	}
+
+}
