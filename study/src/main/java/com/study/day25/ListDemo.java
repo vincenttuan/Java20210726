17415f0c7f124f6a3034523f023713ed94e50d22
vincent <vincentjava@yahoo.com.tw>
2021-08-06 13:26:47 +0800

    java

--- study/src/main/java/com/study/day25/ListDemo.java
+++ study/src/main/java/com/study/day25/ListDemo.java
@@ -13,11 +13,17 @@ public class ListDemo {
 		// 可以放重複資料
 		List<Integer> nums = new LinkedList<>();
 		// 0~9 任意取 4 個 (四星彩 資料可重複)
+		IntConsumer consumer1 = i -> {
+			int n = random.nextInt(10); // 0-9
+			nums.add(n);
+		};
+		IntConsumer consumer2 = i -> {
+			int n = random.nextInt(100); // 0-99
+			nums.add(n);
+		};
+		
 		IntStream.rangeClosed(1, 4)
-			.forEach(i -> {
-				int n = random.nextInt(10); // 0-9
-				nums.add(n);
-			});
+			.forEach(consumer1);
 		
 		System.out.println(nums);
 
