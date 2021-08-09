7a42a4051940385b4a78b1cc1d7291b2e198a229
vincent <vincentjava@yahoo.com.tw>
2021-08-09 14:32:08 +0800

    java

---
+++ study/src/main/java/com/study/day26/TryCatchDemo7.java
@@ -0,0 +1,13 @@
+package com.study.day26;
+
+public class TryCatchDemo7 {
+
+	public static void main(String[] args) {
+		// 寫一個 boolean loginCheck(String username, String password)
+		// 若輸入 loginCheck("John", "1234") 則得到 true
+		// 若輸入 loginCheck("Mary", "1111") 則得到 false
+		// 若輸入 loginCheck("Helen", "1111") 則得到 false
+
+	}
+
+}
