9cceb1a99c909c9513826ed0f3d457dd5a39e643
vincent <vincentjava@yahoo.com.tw>
2021-08-09 10:28:40 +0800

    java

--- study/src/main/java/com/study/day26/TryCatchDemo1.java
+++ study/src/main/java/com/study/day26/TryCatchDemo1.java
@@ -13,9 +13,14 @@ public class TryCatchDemo1 {
 		System.out.print("假設 分子=10 請輸入分母: ");
 		int result = 0; // 結果
 		int x = 10; // 分子
-		int y = scanner.nextInt(); // 分母
-		result = x / y;
-		System.out.printf("x=%d y=%d result=%d\n", x, y, result);
-		
+		try {
+			int y = scanner.nextInt(); // 分母
+			result = x / y;
+			System.out.printf("x=%d y=%d result=%d\n", x, y, result);
+		} catch (Exception e) {
+			System.out.println("請輸入數字");
+			input(); // 重新執行 input() 這種寫法叫做遞迴(自己呼叫自己)
+		}
+		System.out.println("結束");
 	}
 }
