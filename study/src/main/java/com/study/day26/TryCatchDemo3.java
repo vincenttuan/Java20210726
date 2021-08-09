e2ef8c0310f6d6050a8968dfe89541beec45eca0
vincent <vincentjava@yahoo.com.tw>
2021-08-09 11:32:17 +0800

    java

--- study/src/main/java/com/study/day26/TryCatchDemo3.java
+++ study/src/main/java/com/study/day26/TryCatchDemo3.java
@@ -13,14 +13,25 @@ public class TryCatchDemo3 {
 	public static void printData(String fileName) {
 		// 建立檔案
 		File file = new File(fileName);
+		Scanner scanner = null;
 		try {
-			Scanner scanner = new Scanner(file);
+			// useDelimiter 資料的分隔符號[,]  預設是[空白]
+			scanner = new Scanner(file).useDelimiter(",|-");
 		} catch (FileNotFoundException e) {
 			e.printStackTrace(System.out);
 			System.out.printf("%s 檔案找不到\n", fileName);
 			return;
 		}
 		System.out.printf("%s 檔案找到了\n", fileName);
+		// John,170.0,60,學生身高體重資料
+		if(scanner != null) {
+			String name = scanner.next(); 
+			double height = scanner.nextDouble();
+			int weight = scanner.nextInt();
+			String other = scanner.nextLine(); // 剩下一直取到尾端 \n
+			System.out.printf("%s %.1f %d %s\n", name, height, weight, other);
+			 
+		}
 	}
 
 }
