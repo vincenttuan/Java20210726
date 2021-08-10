2db5311a428517255151cb01bba37e06849c32ce
vincent <vincentjava@yahoo.com.tw>
2021-08-10 15:57:46 +0800

    java

---
+++ study/src/main/java/com/study/day32/ATM.java
@@ -0,0 +1,18 @@
+package com.study.day32;
+
+public class ATM {
+
+	public static void main(String[] args) {
+		Account account = new Account(10000);
+		// 提款工作
+		Withdraw w5 = new Withdraw(account, 5000);
+		Withdraw w4 = new Withdraw(account, 4000);
+		Withdraw w3 = new Withdraw(account, 3000);
+		
+		// 找人提款
+		
+		
+		
+	}
+
+}
--- study/src/main/java/com/study/day32/Account.java
+++ study/src/main/java/com/study/day32/Account.java
@@ -17,7 +17,14 @@ public class Account {
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 進來提款\n", tName);
 		
+		int cur_balance = balance; // 取得最新帳戶餘額
 		for(int i=0;i<999999999;i++); // 模擬提款所花費的時間
+		if(cur_balance >= amount) { // 最新帳戶餘額 >= 提款金額
+			balance = cur_balance - amount;
+			System.out.printf("%s 提 $%d 成功, 餘額: $%d\n", tName, amount, balance);
+		} else {
+			System.out.printf("%s 提 $%d 失敗(餘額不足), 餘額: $%d\n", tName, amount, balance);
+		}
 		
 		System.out.printf("%s 離開提款\n", tName);
 	}
