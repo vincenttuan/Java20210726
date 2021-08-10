26c0423f8631ba02ff65b9619b152e1e5509281b
vincent <vincentjava@yahoo.com.tw>
2021-08-10 15:37:58 +0800

    java

--- study/src/main/java/com/study/day31/CarMain.java
+++ study/src/main/java/com/study/day31/CarMain.java
@@ -6,11 +6,12 @@ public class CarMain {
 
 	public static void main(String[] args) {
 		int n = 3;
-		CyclicBarrier cyclicBarrier = new CyclicBarrier(n);
-		new Car(cyclicBarrier, "A").start();
-		new Car(cyclicBarrier, "B").start();
-		new Car(cyclicBarrier, "C").start();
-
+		Runnable eat = () -> System.out.println("吃中餐...");
+		CyclicBarrier cyclicBarrier = new CyclicBarrier(n, eat);
+		new Car(cyclicBarrier, "A1").start();
+		new Car(cyclicBarrier, "B1").start();
+		new Car(cyclicBarrier, "C1").start();
+		
 	}
 
 }
