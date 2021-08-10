611e222ced2ed322f178778673e6d7694ac33845
vincent <vincentjava@yahoo.com.tw>
2021-08-10 16:03:54 +0800

    java

--- study/src/main/java/com/study/day32/ATM.java
+++ study/src/main/java/com/study/day32/ATM.java
@@ -10,8 +10,12 @@ public class ATM {
 		Withdraw w3 = new Withdraw(account, 3000);
 		
 		// 找人提款
-		
-		
+		Thread t1 = new Thread(w5, "小明");
+		Thread t2 = new Thread(w4, "小華");
+		Thread t3 = new Thread(w3, "小英");
+		t1.start();
+		t2.start();
+		t3.start();
 		
 	}
 
--- study/src/main/java/com/study/day32/Account.java
+++ study/src/main/java/com/study/day32/Account.java
@@ -13,7 +13,7 @@ public class Account {
 	}
 	
 	// withdraw 提款方法, amount 提款金額
-	public void withdraw(int amount) { 
+	public synchronized void withdraw(int amount) { 
 		String tName = Thread.currentThread().getName();
 		System.out.printf("%s 進來提款\n", tName);
 		
