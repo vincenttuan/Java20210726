862175ee047bc2f56797d72309161d510a371ddf
vincent <vincentjava@yahoo.com.tw>
2021-08-11 14:19:43 +0800

    java

--- study/src/main/java/com/io/FileOrDir.java
+++ study/src/main/java/com/io/FileOrDir.java
@@ -9,6 +9,7 @@ public class FileOrDir {
 		String[] names = path.list();
 		
 		for(String name : names) {
+			// 組合成完整路徑
 			File f = new File(path + File.separator + name);
 			String value = f.isDirectory() ? "目錄" : "檔案";
 			System.out.println(name + " 是" + value);
