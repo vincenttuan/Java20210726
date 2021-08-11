c6f64e65b296b99ad85f61d4760f605c9a35c40a
vincent <vincentjava@yahoo.com.tw>
2021-08-11 14:35:17 +0800

    java

--- study/src/main/java/com/io/WriteTextToFileDemo.java
+++ study/src/main/java/com/io/WriteTextToFileDemo.java
@@ -2,6 +2,7 @@ package com.io;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 public class WriteTextToFileDemo {
 
@@ -12,10 +13,14 @@ public class WriteTextToFileDemo {
 		
 		FileWriter fw = null;
 		try {
+			// true 保留原檔案內容, false(預設) 清除原檔案內容
+			//fw = new FileWriter("src/main/java/com/io/data/hello.txt", true);
+			//fw = new FileWriter("src/main/java/com/io/data/hello.txt", false);
 			fw = new FileWriter("src/main/java/com/io/data/hello.txt");
 			fw.write(data1);
 			fw.write("\r\n"); // 斷行
 			fw.write(data2);
+			fw.write("\r\n"); // 斷行
 			System.out.println("寫入成功 !");
 		} catch (IOException e) {
 			e.printStackTrace();
