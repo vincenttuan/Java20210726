62539b5cb351604eff671c28508a1cdeba213a61
vincent <vincentjava@yahoo.com.tw>
2021-08-12 12:04:06 +0800

    java

--- study/src/main/java/com/nio/OpenWeather.java
+++ study/src/main/java/com/nio/OpenWeather.java
@@ -5,6 +5,11 @@ import java.net.URL;
 
 import org.apache.commons.io.IOUtils;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 public class OpenWeather {
 	private static final String KEY = "ab99f5242005a98291f643ea873b363d";
 	private static final String WEATHER_PATH = "https://api.openweathermap.org/data/2.5/weather?q=%s,%s&appid=%s";
@@ -17,6 +22,24 @@ public class OpenWeather {
 		// 將 InputStream 轉 String
 		String jsonString = IOUtils.toString(inputStream, "UTF-8");
 		System.out.println(jsonString);
+		// 分析 json
+		JsonElement jsonElement = JsonParser.parseString(jsonString);
+		JsonObject root = jsonElement.getAsJsonObject();
+		JsonObject main = root.getAsJsonObject("main");
+		System.out.println(main);
+		double temp = main.get("temp").getAsDouble();
+		System.out.println(temp);
+		temp -= 273.15;
+		System.out.println(temp);
+		// 溫度=? 體感=? 濕度=?
+		
+		JsonArray weather = root.getAsJsonArray("weather");
+		String description = weather.get(0).getAsJsonObject().get("description").getAsString();
+		System.out.println(description);
+		
+		// homework 將 icon 抓下來並儲存
+		
+		
 	}
 
 }
