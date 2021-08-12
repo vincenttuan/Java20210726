569bfd4f4727e5d6bb15a3f7328a2540eb567855
vincent <vincentjava@yahoo.com.tw>
2021-08-12 09:42:47 +0800

    java

--- study/src/main/java/com/nio/FilesReadDemo.java
+++ study/src/main/java/com/nio/FilesReadDemo.java
@@ -4,18 +4,27 @@ import java.io.BufferedReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class FilesReadDemo {
 
 	public static void main(String[] args) throws Exception {
 		Path path = Paths.get("src/main/java/com/nio/salary.txt");
+		
 		// 1. Files.newBufferedReader
-		System.out.println("---- 1. Files.newBufferedReader ----");
+		System.out.println("-- 1. Files.newBufferedReader --");
 		try(BufferedReader reader = Files.newBufferedReader(path)) {
 			// BufferedReader 的優勢就是可以逐行讀取, 增加執行效率
 			reader.lines().forEach(System.out::println);
 		} catch (Exception e) {
 		}
+		System.out.println();
+		
+		// 2. Files.readAllLines (得到 List 集合)
+		System.out.println("-- 2. Files.readAllLines --");
+		List<String> list = Files.readAllLines(path);
+		System.out.println(list);
+		list.forEach(System.out::println);
 		System.out.println();
 		
 		
